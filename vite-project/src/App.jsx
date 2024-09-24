10e0b883a85df24bbaedf5c3d9034c008cd697eb
Sumayya-Afrin <sumayyafrin.mohammad@proclink.com>
2024-09-24 09:55:45 +0530

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,22 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Fruits from './components/Fruits'
+import './components/Hello'
+
+
+
+function App() {
+
+
+  return (
+    <>
+   <Fruits></Fruits>
+    
+    </>
+
+  )
+}
+
+export default App
---
+++ vite-project/src/components/Hello.jsx
@@ -0,0 +1,9 @@
+// eslint-disable-next-line react/prop-types
+function Hello({name , phone ,seatNumber})
+{
+    return (
+        <h1>Hello {name} , {phone} , {seatNumber}</h1>
+    )
+}
+
+export default Hello
